4ed2737006ee0af603ee2a4df5b87b621d88765e
Mhdowner12 <anshifkp1894@gmail.com>
2024-10-02 06:38:50 +0530

    Update auto.py

--- auto.py
+++ auto.py
@@ -32,8 +32,8 @@ def load_credentials(session_name):
 
 # Function to display banner
 def display_banner():
-    print(Fore.RED + pyfiglet.figlet_format("LEGITDEALS9"))
-    print(Fore.GREEN + "Made by @Legitdeals9\n")
+    print(Fore.RED + pyfiglet.figlet_format("MEGIX OTT"))
+    print(Fore.GREEN + "Made by @Megix_OTT\n")
 
 # Function to login and forward messages
 async def login_and_forward(api_id, api_hash, phone_number, session_name):
